e2d2fda56a407f8fb38dcd0ec47ceed6e55c5767
Sushant Kumar <sushant.kum96@gmail.com>
2020-10-06 11:49:45 +0530

    chore: nx migrate

--- apps/api/auth/jest.config.js
+++ apps/api/auth/jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  name: 'api-auth',
-  preset: '../../../jest.config.js',
+  preset: '../../../jest.preset.js',
   coverageDirectory: '../../../coverage/apps/api/auth',
+  globals: { 'ts-jest': { tsConfig: '<rootDir>/tsconfig.spec.json' } },
+  displayName: 'api-auth',
 };
--- libs/backend/db/user/jest.config.js
+++ libs/backend/db/user/jest.config.js
@@ -1,10 +1,11 @@
 module.exports = {
-  name: 'backend-db-user',
-  preset: '../../../../jest.config.js',
+  preset: '../../../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
   coverageDirectory: '../../../../coverage/libs/backend/db/user',
+  globals: { 'ts-jest': { tsConfig: '<rootDir>/tsconfig.spec.json' } },
+  displayName: 'backend-db-user',
 };
